8414b72194d621ddc8c0300e5a31796efc4cc32e
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2020-04-29 04:39:34 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import "nekonenene/youtube_description_updater/src/updater"
+import "github.com/nekonenene/youtube_description_updater/src/updater"
 
 func main() {
 	updater.ParseParameters()
